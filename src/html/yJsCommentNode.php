e279daba9c0b7ee1c77674f926b5aff16289fc35
olamedia <olamedia@gmail.com>
2011-04-24 20:35:53 +0600

    js comment node

--- src/html/yJsCommentNode.php
+++ src/html/yJsCommentNode.php
@@ -1,8 +1,13 @@
 <?php
 
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * This file is part of the yuki package.
+ * Copyright (c) 2011 olamedia <olamedia@gmail.com>
+ *
+ * This source code is release under the MIT License.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 /**
@@ -10,9 +15,6 @@
  *
  * @package yuki
  * @subpackage html
- * @version SVN: $Id$
- * @revision SVN: $Revision$
- * @date $Date$
  * @author olamedia
  * @license http://www.opensource.org/licenses/mit-license.php MIT
  */
